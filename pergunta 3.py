d7adbe36a9c1e3a82fae5f075b2de52a1e909184
Nathan Dallaqua <npdallaqua@gmail.com>
2023-02-04 13:37:21 -0300

    pergunta 3.py
    Pergunta numero 3 do teste da DataCake

---
+++ pergunta 3.py
@@ -0,0 +1,29 @@
+from time import sleep
+
+sim=[]
+arr=[1, 15, 2, 7, 2, 5, 7, 1, 4]
+print('Esté é o array informado:[1, 15, 2, 7, 2, 5, 7, 1, 4]')
+sleep(1.5)
+
+
+#Adicionar os valores nesta nova lista
+nova=[]
+
+#Informar o valor desejado
+valor=int(input('Informe o argumento para ser somado: '))
+
+
+#Adicionando os números na nova lista
+for n in arr:
+    nova.append([n])
+
+#Realizando o cálculo
+for x in range (0,9):
+    for c in range (0,9):
+        if sum(nova[x] + nova[c]) == valor:
+            sim.append(+1)
+            break
+if len(sim) > 0:
+    print('True')
+else:
+    print('False')
