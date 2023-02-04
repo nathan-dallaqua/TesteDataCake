6fe2f4bc428147f5876d2a53b400a3594b3bbd8a
Nathan Dallaqua <npdallaqua@gmail.com>
2023-02-04 18:51:28 -0300

    Update pergunta 1.py

--- pergunta 1.py
+++ pergunta 1.py
@@ -1,30 +1,30 @@
 from time import sleep
 #Array solicitada
-numeros=[2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+arr=[2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
 
 #Lista que irá receber os números 1 retirados
-um=[]
+num1=[]
 
 
 #Apresentando os números na tela
 print(f'''Os números listados são:
-{numeros}''')
+{arr}''')
 sleep(1)
 
 #Ler a lista e retirar todos os numeros 1
-for n in numeros:
-  if n == 1:
-    numeros.remove(1)
-    um.append(1)
+for n in range(0,14):
+  if 1 in arr:
+    arr.remove(1)
+    num1.append(1)
 
 #Adiciona-los denovo no início
-for n in um:
-  numeros.insert(0,1)
+for n in num1:
+  arr.insert(0,1)
 
 
 
 #Resultado final
 print(' ')
 print(f'''Ordenando apenas o número 1 na array temos:
-{numeros}
+{arr}
 ''')
