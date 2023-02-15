b27da31b5d5289f689322a9d5286bbe2099a5976
Nathan Dallaqua <122532438+nathan-dallaqua@users.noreply.github.com>
2023-02-15 16:23:31 -0300

    Update pergunta 2.py

--- pergunta 2.py
+++ pergunta 2.py
@@ -1,4 +1,4 @@
-
+from time import sleep
 class Node:
     def __init__(self, data):
         self.data = data
@@ -25,41 +25,54 @@ if __name__ == '__main__':
     tree.root.right.left.left = Node('Laranja')
     tree.root.right.left.left.left = Node('Banana')
     tree.root.right.left.left.right = Node('Cebola')
+
+while True:  
+  fruta=int(input('''Temos uma árvore binária onde possui as frutas:
+  1 - Maçã
+  2 - Morango
+  3 - Goiaba
+  4 - Limão
+  5 - Pera
+  6 - Abacaxi
+  7 - Laranja
+  8 - Banana
+  9 - Cebola
+  Informe o número da fruta que deseja e elá será buscada nesta árvore:
+  '''))
   
-fruta=int(input('''Temos uma árvore binária onde possui as frutas:
-1 - Maçã
-2 - Morango
-3 - Goiaba
-4 - Limão
-5 - Pera
-6 - Abacaxi
-7 - Laranja
-8 - Banana
-9 - Cebola
-Informe o número da fruta que deseja e elá será buscada nesta árvore:
-'''))
-
-
-if fruta == 2:
-  print(tree.root, '=>', tree.root.left)
   
-elif fruta == 3:
-  print(tree.root,'=>', tree.root.left, '=>', tree.root.left.left)
+  if fruta == 2:
+    print(tree.root, '=>', tree.root.left)
+    break
+    
+  elif fruta == 3:
+    print(tree.root,'=>', tree.root.left, '=>', tree.root.left.left)
+    break
+    
+  elif fruta == 4:
+    print(tree.root,'=>', tree.root.left, '=>', tree.root.left.right)
+    break
+    
+  elif fruta == 5:
+    print(tree.root,'=>', tree.root.right)
+    break
+    
+  elif fruta == 6:
+    print(tree.root,'=>', tree.root.right,'=>', tree.root.right.left)
+    break
 
-elif fruta == 4:
-  print(tree.root,'=>', tree.root.left, '=>', tree.root.left.right)
+  elif fruta == 7:
+    print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left)
+    break
 
-elif fruta == 5:
-  print(tree.root,'=>', tree.root.right)
+  elif fruta == 8:
+    print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left,'=>',tree.root.right.left.left.left )
+    break
+    
+  elif fruta == 9:
+    print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left,'=>',tree.root.right.left.left.right)
+    break
 
-elif fruta == 6:
-  print(tree.root,'=>', tree.root.right,'=>', tree.root.right.left)
-
-elif fruta == 7:
-  print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left)
-
-elif fruta == 8:
-  print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left,'=>',tree.root.right.left.left.left )
-
-elif fruta == 9:
-  print (tree.root,'=>', tree.root.right,'=>', tree.root.right.left, '=>', tree.root.right.left.left,'=>',tree.root.right.left.left.right)
+  else:
+    print('Valor inválido tente novamente....')
+    sleep(1)
